697303d5955627ac033f82e24d74b074e34d438d
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-06-04 11:40:58 +0100

    Implement homepage and companion form

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-3xl underline text-yellow-400">
-        Welcome to my SaaS App
-      </h1>
+    <main>
+      <h1>Popular Companions</h1>
 
-        <Button>
-            Get Started
-        </Button>
-    </div>
-  )
-}
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy  the Math Wizard"
+          topic="Derivatives & Integrals"
+          subject="Mathematics"
+          duration={30}
+          color="#FFB6C1"
+        />
+        <CompanionCard
+          id="789"
+          name="Lingo the Vocabulary Virtuoso"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#ADD8E6"
+        />
+      </section>
 
-export default Page
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
